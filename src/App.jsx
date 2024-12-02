cc2acf1b4a4d7e54217596606e67fa2b557530df
siddhesh <siddhesh85@gmail.com>
2024-12-02 15:26:41 +0530

    Basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// packages
+import { Route, Routes } from 'react-router-dom';
+
+// pages
+import OverviewPage from './pages/OverviewPage';
+import ProductsPage from './pages/ProductsPage';
+
+// components
+import Sidebar from './components/Sidebar';
+
+const App = () => {
+  return (
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+      {/* BG */}
+      <div className='fixed inset-0 z-0'>
+        <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80'></div>
+        <div className='absolute inset-0 backdrop-blur-sm'></div>
+      </div>
+
+      {/* Sidebar */}
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<OverviewPage />}></Route>
+        <Route path='/products' element={<ProductsPage />}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,7 @@
+const OverviewPage = () => {
+  return (
+    <div>Overview Page</div>
+  );
+}
+
+export default OverviewPage;
